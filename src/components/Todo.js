c808522ced94b8848133bb25611c36d6f41e8180
Patryk Stanek <p.stanek3@gmail.com>
2019-05-14 16:11:32 +0200

    fixed key value

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Todo = props => (
-    <li key={props.id}>
+    <li>
         <span onClick={() => props.remove(props.id)}>╳</span>
         <p>{props.text}</p>
     </li>
